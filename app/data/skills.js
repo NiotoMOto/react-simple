c70b6e0bec23f12a3d9382e3fdd07efb09e5aa23
Nioto <antoine.guillemoto@gmail.com>
2016-12-12 14:43:25 +0100

    Create skills.js

---
+++ app/data/skills.js
@@ -0,0 +1,52 @@
+'use strict';
+
+export default [
+  {
+    type: 'js',
+    value: 8,
+    text: 'angularks lorem ipsum dolor sit amet, consectetur adipisicing eli',
+    title : 'angularjs'
+  },
+  {
+    type: 'js',
+    value: 5,
+    text: 'Reactjs lorem ipsum dolor sit amet, consectetur adipisicing eli',
+    title : 'reactjs'
+  },
+  {
+    type: 'env',
+    value: 8,
+    text: 'git lorem ipsum dolor sit amet, consectetur adipisicing eli',
+    title : 'git'
+  },
+  {
+    type: 'env',
+    value: 8,
+    text: 'git lorem ipsum dolor sit amet, consectetur adipisicing eli',
+    title : 'git'
+  },
+  {
+    type: 'env',
+    value: 8,
+    text: 'git lorem ipsum dolor sit amet, consectetur adipisicing eli',
+    title : 'git'
+  },
+  {
+    type: 'env',
+    value: 8,
+    text: 'git lorem ipsum dolor sit amet, consectetur adipisicing eli',
+    title : 'git'
+  },
+  {
+    type: 'env',
+    value: 8,
+    text: 'git lorem ipsum dolor sit amet, consectetur adipisicing eli',
+    title : 'git'
+  },
+  {
+    type: 'env',
+    value: 8,
+    text: 'git lorem ipsum dolor sit amet, consectetur adipisicing eli',
+    title : 'git'
+  },
+]
